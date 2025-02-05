4e6340e2a20f0fdff8eee762bd5f5e46ee38cac7
Pekko <pekko.eskola@aalto.fi>
2025-02-05 18:29:09 +0200

    Removed db test from index.js after sanity check that dev setup works

--- index.ts
+++ index.ts
@@ -1,23 +1,7 @@
 import "dotenv/config";
 import express = require("express");
 
-import prisma from "./prisma/client";
-
 const app = express();
-
-const dbtest = async () => {
-  await prisma.user.create({
-    data: {
-      username: "Alice",
-      password: "password",
-    },
-  });
-
-  const users = await prisma.user.findMany();
-  console.log(users);
-};
-
-void dbtest();
 
 app.get("/", (_req, res) => {
   res.send("serves nothing at root, api is at /api");

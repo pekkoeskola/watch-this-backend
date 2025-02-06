99ba2b45605287c8b4335f6bb052442fa84d82eb
pekkoeskola <pekko.eskola@hotmail.fi>
2025-02-06 23:35:35 +0200

    Remove unnecessary comments from valkey client conifg

--- valkey/client.ts
+++ valkey/client.ts
@@ -1,5 +1,4 @@
 import { GlideClient } from "@valkey/valkey-glide";
-// When Valkey is in standalone mode, add address of the primary node, and any replicas you'd like to be able to read from.
 
 //TODO: configure production setup
 const addresses = [
@@ -8,11 +7,9 @@ const addresses = [
         port: 6379,
     },
 ];
-// Check `GlideClientConfiguration/GlideClusterClientConfiguration` for additional options.
 
 const client = GlideClient.createClient({
     addresses: addresses,
-    // if the server uses TLS, you'll need to enable it. Otherwise, the connection attempt will time out silently.
     // useTLS: true,
     clientName: "test_standalone_client",
 });

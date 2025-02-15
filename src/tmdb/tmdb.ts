523c288a2d87b34a72f93ac5d71382c6a6b4d6c4
pekkoeskola <pekko.eskola@hotmail.fi>
2025-02-16 01:19:17 +0200

    tmdb api access functions now throw TMDBAPIErrors when the tmdb library throws an error

--- src/tmdb/tmdb.ts
+++ src/tmdb/tmdb.ts
@@ -1,11 +1,24 @@
+import { TMDBAPIError } from "../utils/errors.js";
 import moviedb from "./clientSetup.js";
 
 const searchByString = (searchString: string) => {
-  return moviedb.searchMovie({ query: searchString, page: 1 });
+  try {
+    return moviedb.searchMovie({ query: searchString, page: 1 });
+  } catch (e) {
+    void e;
+    throw new TMDBAPIError("Error searching for movie");
+  }
 };
 
 const getMovieDetails = (id: number) => {
-  return moviedb.movieInfo(id);
+  try {
+    return moviedb.movieInfo(id);
+  } catch (e) {
+    void e;
+    throw new TMDBAPIError(
+      "Error getting movie details: likely could not find movie with that id",
+    );
+  }
 };
 
 export default { searchByString, getMovieDetails };

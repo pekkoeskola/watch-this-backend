6c5bceed3cdac9ca0b8605865e74a95cc6e757c0
pekkoeskola <pekko.eskola@hotmail.fi>
2025-02-18 02:41:43 +0200

    Added logger functions to use from now on in place of console.log and console.error

---
+++ src/utils/logger.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const logInfo = (message: any) => {
+  console.log(message);
+};
+
+export const logError = (message: any) => {
+  console.error(message);
+};
